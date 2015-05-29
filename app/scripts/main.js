d3f9cc5b29160f861627acafaba65712b17287e9
Matt Dougherty <mattz325@gmail.com>
2015-05-29 17:19:06 -0400

    Initial dist subtree commit

--- app/scripts/main.js
+++
@@ -1,9 +0,0 @@
-Parse.initialize("qStdqzNUq7WnxxK5mu1VHFHVjU3JXo4frzcxeRmH", "s6f6SjFKLmGVlA7oqHfecJCE7u1K0Am9Yzb9hE6t");
-
-filepicker.setKey("AFLbZroQeC5fnjZH5RpUAz");
-
-$('.logout').click(function() {
-	Parse.User.logOut();
-	router.navigate("home", {trigger: true})
-	alert('user has logged out!')
-});
